c9a37c9d5e64c77195c2ba6cd661fcd7abcd95ea
zhangchenyan <1065108500@qq.com>
2016-03-23 13:35:30 +0800

    Revert "解析二维码"
    This reverts commit f749db774613507611cbf457ea9d9e2d0ccad539.

--- src/main/java/cn/edu/seu/ytw/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.seu.ytw.dao;
-
-import cn.edu.seu.ytw.bean.User;
-import cn.edu.seu.ytw.common.BaseDAO;
-
-public interface UserDAO extends BaseDAO<User, Integer>{
-
-}
--- src/main/java/cn/edu/seu/ytw/service/InfoService.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.seu.ytw.service;
-import cn.edu.seu.ytw.bean.User;
-import cn.edu.seu.ytw.bean.Order;
-public interface InfoService {
-  
-	public boolean createOrder(String sender,String senderphone,String idnum,String sAddr,String recipient,String recipientphone,String rAddr);
-	public String getAddbyID(int id);
-}
